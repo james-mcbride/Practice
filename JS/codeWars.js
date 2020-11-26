5ee4c3b5d03eae30a1341a054b42e79e8ad617ee
james McBride <james.m.mcbride15@gmail.com>
2020-11-25 18:28:55 -0600

    adding updates to 5th weekly coding challenge

--- JS/codeWars.js
+++ JS/codeWars.js
@@ -42,50 +42,136 @@
 //
 // Some examples:
 
+//first attempt
+//const maxMirror = (array) => {
+//     console.log(array);
+//     var counter=0;
+//     var scoreKeeperArray=[];
+//     var forwardIndex=0;
+//     var backwardIndex=0;
+//
+//     for (var i=0; i<array.length; i++){
+//         var exitInnerLoop=0;
+//         for (var j=array.length-1; j>=0; j--){
+//             if (exitInnerLoop>0){
+//                 continue;
+//             }
+//             //console.log("var i = "+ i,"arr[i] = "+ array[i], "var j= "+ j, "array[j]= "+array[j] );
+//             if (array[j]===array[i] && i!==j){
+//                 if (counter===0) {
+//                     counter++;
+//                     forwardIndex = i;
+//                     backwardIndex = j;
+//                     exitInnerLoop++;
+//                     //console.log("we have a match!")
+//                 } else if(i===forwardIndex+1 && j===backwardIndex-1){
+//                     counter++;
+//                     forwardIndex=i;
+//                     backwardIndex=j;
+//                     exitInnerLoop++
+//                     //console.log("We have another match!")
+//                 } else{
+//                     if (forward)
+//                     scoreKeeperArray.push(counter);
+//                     counter=1;
+//                     forwardIndex=i;
+//                     backwardIndex=j;
+//                     exitInnerLoop++;
+//                     //console.log("well, starting over again with a first match")
+//                 }
+//             } else if (array[j]===array[i] && i===j){
+//                 if(array[i-1]===array[i+1]){
+//                     counter++;
+//                     forwardIndex=i;
+//                     backwardIndex=j;
+//                     exitInnerLoop++
+//                    // console.log("We have a middle match!")
+//                 }
+//             }
+//         }
+//     }
+//     scoreKeeperArray.push(counter);
+//     if (Math.max(...scoreKeeperArray)>1){
+//         return Math.max(...scoreKeeperArray);
+//     }else{
+//         return 0;
+//     }
+// }
+
 const maxMirror = (array) => {
     console.log(array);
+    //counter will count current mirror streak
     var counter=0;
+    //scoreKeeperArray will log each mirror streak
     var scoreKeeperArray=[];
+    //forwardIndex will hold the index i of the last mirrored element in the outer forward loop
     var forwardIndex=0;
+    //backward index will hold the index j of the last mirrored element in the inner backwards loop
     var backwardIndex=0;
 
     for (var i=0; i<array.length; i++){
+        //exitInnerLoop will skip the remainder of the inner loop after a match is found.
         var exitInnerLoop=0;
         for (var j=array.length-1; j>=0; j--){
+            //will exit out of current innerLoop if a current mirror is being pursued
+
+            if (counter>0 && exitInnerLoop===0){
+                j=backwardIndex-1;
+            }
             if (exitInnerLoop>0){
+                console.log("We have found a match and are skipping to the next i-value")
                 continue;
             }
-            console.log("var i = "+ i,"arr[i] = "+ array[i], "var j= "+ j, "array[j]= "+array[j] );
-            if (array[j]===array[i] && i!==j){
+            console.log("var i = "+ i,"arr[i] = "+ array[i], "var j= "+ j, "array[j]= "+array[j], "forward index = "+ forwardIndex, "backward index = "+backwardIndex, "counter = "+counter );
+            //skip loop when i and j land on the same index and you are not in the middle of a mirror streak.
+            if (array[j]===array[i] && i===j &&counter==0){
+                continue;
+            } else if (array[j]===array[i]){
                 if (counter===0) {
-                    counter++;
-                    forwardIndex = i;
-                    backwardIndex = j;
-                    exitInnerLoop++;
-                    console.log("we have a match!")
+                    if (array[i+1]===array[j-1]){
+                        ++counter;
+                        forwardIndex = i;
+                        backwardIndex = j;
+                        exitInnerLoop++;
+                        console.log("we have a match!",  "counter= "+counter)
+                    } else{
+                        scoreKeeperArray.push(1);
+                        counter=0;
+                        exitInnerLoop=0;
+                        continue;
+                    }
+
                 } else if(i===forwardIndex+1 && j===backwardIndex-1){
-                    counter++;
-                    forwardIndex=i;
-                    backwardIndex=j;
-                    exitInnerLoop++
-                    console.log("We have another match!")
+                    console.log("array[i+1]"+array[i+1], "array[j-1" )
+                    if (array[i+1]===array[j-1]) {
+                        ++counter;
+                        forwardIndex = i;
+                        backwardIndex = j;
+                        exitInnerLoop++;
+                        console.log("We have another match and will keep going!", "counter= "+counter)
+                    } else{
+                        ++counter;
+                        console.log("we have another match, but the buck stops here.", "counter = " +counter)
+                        scoreKeeperArray.push(counter);
+                        counter=0;
+                        exitInnerLoop=0;
+                        continue;
+                    }
                 } else{
-                    scoreKeeperArray.push(counter);
-                    counter=1;
-                    forwardIndex=i;
-                    backwardIndex=j;
-                    exitInnerLoop++;
-                    console.log("well, starting over again with a first match")
+                    continue;
+                    // scoreKeeperArray.push(counter);
+                    // counter=1;
+                    // forwardIndex=i;
+                    // backwardIndex=j;
+                    // exitInnerLoop++;
+                    // console.log("well, starting over again with a first match")
                 }
-            } else if (array[j]===array[i] && i===j){
-                if(array[i-1]===array[i+1]){
-                    counter++;
-                    forwardIndex=i;
-                    backwardIndex=j;
-                    exitInnerLoop++
-                    console.log("We have a middle match!")
-                }
+            } else{
+                counter=0;
+                backwardIndex=0;
+                forwardIndex=0;
             }
+
         }
     }
     scoreKeeperArray.push(counter);
@@ -95,4 +181,4 @@ const maxMirror = (array) => {
         return 0;
     }
 }
-console.log(maxMirror([ 10, 5, 10, 2, 1, 7, 3, 5, 6, 7, 5, 9, 7, 5, 5 ]))
+console.log(maxMirror([ 1, 2, 3, 8, 9, 3, 2, 1, 9, 8 ]))
